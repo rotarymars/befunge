491d437f52b0f402bb03ac7bba07b8b9e2ab1214
rotarymars <s.goto2050@gmail.com>
2026-07-22 20:56:28 +0900

    a hash function of Vec

--- lib/code.hpp
+++ lib/code.hpp
@@ -7,10 +7,12 @@
  */
 
 #include <cstdint>
+#include <functional>
 
 class Vec {
   public:
     int64_t x, y;
+    bool operator==(const Vec&) const = default;
 };
 
 constexpr int64_t CHUNK_SIZE = (1 << 6);
@@ -21,4 +23,14 @@ class Block {
   public:
     Block();
 };
+
+template <>
+struct std::hash<Vec> {
+  std::size_t operator()(const Vec& v) const noexcept {
+    std::size_t h1 = std::hash<size_t>{}(v.x);
+    std::size_t h2 = std::hash<size_t>{}(v.y);
+    return h1 ^ (h2 + 0x9e3779b97f4a7c15ULL + (h1 << 6) + (h1 >> 2)); // this is a magic hash that works...
+  }
+};
+
 #endif
