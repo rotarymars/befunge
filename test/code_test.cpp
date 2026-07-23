cdba75ba68f00dd0a8b7094cffe294025065cfe6
rotarymars <s.goto2050@gmail.com>
2026-07-23 19:49:58 +0900

    a load test

--- test/code_test.cpp
+++ test/code_test.cpp
@@ -1,5 +1,6 @@
 #include "code.hpp"
 #include "gtest/gtest.h"
+#include <sstream>
 
 TEST(FungeSpace, NegativeCoordinatesRoundTrip) {
     FungeSpace fs;
@@ -26,6 +27,26 @@ TEST(FungeSpace, BoundsTrackUsedRegion) {
     fs.put({4, 1}, 'B');
     EXPECT_EQ(fs.min().x, -1);
     EXPECT_EQ(fs.max().x, 4);
+}
+
+TEST(FungeSpace, LoadsGrid) {
+    std::istringstream src(">123v\n    @\n");
+    FungeSpace fs;
+    fs.load(src);
+    EXPECT_EQ(fs.get({0, 0}), '>');
+    EXPECT_EQ(fs.get({4, 0}), 'v');
+    EXPECT_EQ(fs.get({4, 1}), '@');   // second row: x resets after '\n'
+    EXPECT_EQ(fs.get({1, 1}), 32);    // a space stays 32
+}
+
+TEST(FungeSpace, LoadStripsCarriageReturns) {
+    std::istringstream src(">v\r\n@\r\n");   // Windows CRLF line endings
+    FungeSpace fs;
+    fs.load(src);
+    EXPECT_EQ(fs.get({0, 0}), '>');
+    EXPECT_EQ(fs.get({1, 0}), 'v');
+    EXPECT_EQ(fs.get({2, 0}), 32);    // no stray '\r' stored at end of line
+    EXPECT_EQ(fs.get({0, 1}), '@');   // next row starts at x=0, not shifted by '\r'
 }
 
 TEST(Vec, HashDistinguishesSwappedCoords) {
