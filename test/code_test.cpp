c2213334abeb92b339234c55c1eb48f8ba218877
rotarymars <s.goto2050@gmail.com>
2026-07-23 20:21:13 +0900

    stack tests

--- test/code_test.cpp
+++ test/code_test.cpp
@@ -52,3 +52,62 @@ TEST(FungeSpace, LoadStripsCarriageReturns) {
 TEST(Vec, HashDistinguishesSwappedCoords) {
   EXPECT_NE((std::hash<Vec>{}({3, 5})), (std::hash<Vec>{}({5, 3})));
 }
+
+TEST(Stack, NewStackIsEmpty) {
+  Stack s;
+  EXPECT_TRUE(s.empty());
+  EXPECT_EQ(s.size(), 0u);
+}
+
+TEST(Stack, PopEmptyReturnsZero) {
+  Stack s;
+  EXPECT_EQ(s.pop(), 0); // the critical Befunge rule: underflow yields 0
+  EXPECT_EQ(s.pop(), 0); // still 0, still safe
+  EXPECT_TRUE(s.empty());
+}
+
+TEST(Stack, PeekEmptyReturnsZero) {
+  Stack s;
+  EXPECT_EQ(s.peek(), 0);
+  EXPECT_TRUE(s.empty()); // peek must not consume anything
+}
+
+TEST(Stack, PushPopIsLifo) {
+  Stack s;
+  s.push(1);
+  s.push(2);
+  s.push(3);
+  EXPECT_EQ(s.size(), 3u);
+  EXPECT_EQ(s.pop(), 3);
+  EXPECT_EQ(s.pop(), 2);
+  EXPECT_EQ(s.pop(), 1);
+  EXPECT_EQ(s.pop(), 0); // drained past empty -> 0
+}
+
+TEST(Stack, PeekDoesNotConsume) {
+  Stack s;
+  s.push(42);
+  EXPECT_EQ(s.peek(), 42);
+  EXPECT_EQ(s.peek(), 42); // still there
+  EXPECT_EQ(s.size(), 1u);
+  EXPECT_EQ(s.pop(), 42);
+}
+
+TEST(Stack, HandlesNegativeAndLargeValues) {
+  Stack s;
+  s.push(-7);
+  s.push(INT64_MAX);
+  s.push(INT64_MIN);
+  EXPECT_EQ(s.pop(), INT64_MIN);
+  EXPECT_EQ(s.pop(), INT64_MAX);
+  EXPECT_EQ(s.pop(), -7);
+}
+
+TEST(Stack, RefillAfterUnderflow) {
+  Stack s;
+  s.push(5);
+  EXPECT_EQ(s.pop(), 5);
+  EXPECT_EQ(s.pop(), 0); // underflow
+  s.push(9);             // stack still usable after underflow
+  EXPECT_EQ(s.pop(), 9);
+}
